0e9f97e17a4aca1eac99a1ed84af3a94610a3c15
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2025-01-24 21:35:07 +0000

    ♻️ Cleaned up supabase

--- app/routes/character.$id.settings.tsx
+++ app/routes/character.$id.settings.tsx
@@ -1,7 +1,6 @@
 import { Button, rem, Select, Stack, Switch, Title } from '@mantine/core'
 import { type ActionFunctionArgs, json, type LoaderFunctionArgs, type MetaFunction } from '@remix-run/node'
 import { Form, redirect, useLoaderData } from '@remix-run/react'
-import { getCharacterData } from 'app/.server/data/getCharacterData'
 import { getServerClient } from '~/supabase/getServerClient'
 import { getServiceClient } from '~/supabase/getServiceClient'
 import { getUserId } from '~/supabase/getUserId'
@@ -17,28 +16,32 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
 export async function loader({ params, request }: LoaderFunctionArgs) {
 	const characterId = isNumberParam(params.id, request.headers)
 
-	const serverClient = getServerClient(request)
-	const { supabase, headers } = serverClient
+	const { supabase, headers } = getServerClient(request)
 
-	const {
-		character_name,
-		campaign_id,
-		user_id,
-		visibility
-	} = await getCharacterData('character_name, campaign_id, user_id, visibility', characterId, serverClient)
+	const { data: characterData, error } = await supabase
+		.from('character_info')
+		.select(`
+			characterName: character_name,
+			campaignId: campaign_id,
+			characterUserId: user_id,
+			visibility
+		`)
+		.eq('character_id', characterId)
+	if (error || !characterData.length) throw redirect('/', { headers })
+	const { characterName, campaignId, characterUserId, visibility } = characterData[0]
 
 	const accountUserId = (await getUserId(supabase)).userId
-	if (user_id !== accountUserId) throw redirect(`/character/${characterId}`, { headers })
+	if (characterUserId !== accountUserId) throw redirect(`/character/${characterId}`, { headers })
 
-	const { data, error } = await supabase
+	const { data: campaignData, error: campaignError } = await supabase
 		.from('campaign_info')
 		.select('campaign_id, campaign_name')
-	if (error) throw redirect('/', { headers })
+	if (campaignError) throw redirect('/', { headers })
 
 	return json({
-		characterName: character_name,
-		campaignId: campaign_id,
-		campaigns: data,
+		characterName,
+		campaignId,
+		campaigns: campaignData,
 		visibility
 	}, { headers })
 }
