014afe11b93a9544d23932ea073b3ae782d60ed9
JoelDohmKaratjas <33188727+JoelDohmKaratjas@users.noreply.github.com>
2025-12-19 14:58:39 +0000

    ✨Update character stats

--- src/routes/tabletop/$campaignId.gm/-components/SettingsPanel/UnitsTab/CharacterCard.tsx
+++ src/routes/tabletop/$campaignId.gm/-components/SettingsPanel/UnitsTab/CharacterCard.tsx
@@ -1,6 +1,14 @@
-import { ActionIcon, Avatar, Card, Group, Menu, Stack, Text } from '@mantine/core'
+import { ActionIcon, Avatar, Button, Card, Group, Menu, Modal, NumberInput, type NumberInputProps, Stack, Text } from '@mantine/core'
+import { isInRange, useForm } from '@mantine/form'
+import { useDisclosure } from '@mantine/hooks'
 import { IconPencil, IconTrash, IconX } from '@tabler/icons-react'
+import { useMutation } from '@tanstack/react-query'
+import { createServerFn } from '@tanstack/react-start'
+import { type } from 'arktype'
 import { type Enums } from '~/supabase/databaseTypes'
+import { getServiceClient } from '~/supabase/getServiceClient'
+import { requireGM } from '~/supabase/requireGM'
+import { mutationError } from '~/utils/mutationError'
 
 type CharacterCardProps = {
 	character: {
@@ -27,45 +35,191 @@ type CharacterCardProps = {
 	removeCharacter?: () => void
 }
 
-export default function CharacterCard({
-	character: { type, tabletopCharacterId, characterName, stats, tabletopStats },
-	removeCharacter
-}: CharacterCardProps) {
+export default function CharacterCard({ character, removeCharacter }: CharacterCardProps) {
+	const { type, tabletopCharacterId, characterName, stats, tabletopStats } = character
+
+	const [opened, { open, close }] = useDisclosure(false)
+
 	return (
-		<Card key={tabletopCharacterId} component={Stack} gap={2} align='center' p='sm' bg='dark.5'>
-			<Avatar name={characterName} color='red' />
-			<Text>{characterName}</Text>
-			<Text>Shield: {tabletopStats.shield} [{tabletopStats.trauma}] / {stats.maxShield}</Text>
-			<Text>Health: {tabletopStats.health} [{tabletopStats.wounds}] / {stats.maxHealth}</Text>
-			<Text>
-				<Text span c='blue'>{stats.int}</Text>
-				<Text span> / </Text>
-				<Text span c='red'>{stats.str}</Text>
-				<Text span> / </Text>
-				<Text span c='green'>{stats.dex}</Text>
-			</Text>
-			<Group gap='xs'>
-				<ActionIcon variant='subtle' color='gray'>
-					<IconPencil />
-				</ActionIcon>
-				<Menu>
-					<Menu.Target>
-						<ActionIcon variant='subtle' color='red'>
-							<IconX />
-						</ActionIcon>
-					</Menu.Target>
-					<Menu.Dropdown>
-						<Menu.Label>Are you sure?</Menu.Label>
-						<Menu.Item
-							color='red'
-							leftSection={<IconTrash size={14} />}
-							onClick={() => removeCharacter?.()}
-						>
-							Remove {type === 'HERO' ? 'Hero' : 'Enemy'}
-						</Menu.Item>
-					</Menu.Dropdown>
-				</Menu>
-			</Group>
-		</Card>
+		<>
+			<EditCharacterModal character={character} openedModal={opened} closeModal={close} />
+
+			<Card key={tabletopCharacterId} component={Stack} gap={2} align='center' p='sm' bg='dark.5'>
+				<Avatar name={characterName} color='red' />
+				<Text>{characterName}</Text>
+				<Text>Shield: {tabletopStats.shield} [{tabletopStats.trauma}] / {stats.maxShield}</Text>
+				<Text>Health: {tabletopStats.health} [{tabletopStats.wounds}] / {stats.maxHealth}</Text>
+				<Text>
+					<Text span c='blue'>{stats.int}</Text>
+					<Text span> / </Text>
+					<Text span c='red'>{stats.str}</Text>
+					<Text span> / </Text>
+					<Text span c='green'>{stats.dex}</Text>
+				</Text>
+				<Text>Movement: {tabletopStats.movement} / {stats.maxMovement}</Text>
+				<Group gap='xs'>
+					<ActionIcon variant='subtle' color='gray'>
+						<IconPencil onClick={open} />
+					</ActionIcon>
+					<Menu>
+						<Menu.Target>
+							<ActionIcon variant='subtle' color='red'>
+								<IconX />
+							</ActionIcon>
+						</Menu.Target>
+						<Menu.Dropdown>
+							<Menu.Label>Are you sure?</Menu.Label>
+							<Menu.Item
+								color='red'
+								leftSection={<IconTrash size={14} />}
+								onClick={() => removeCharacter?.()}
+							>
+								Remove {type === 'HERO' ? 'Hero' : 'Enemy'}
+							</Menu.Item>
+						</Menu.Dropdown>
+					</Menu>
+				</Group>
+			</Card>
+		</>
 	)
 }
+
+const numberInputProps: NumberInputProps = {
+	min: 0,
+	allowDecimal: false,
+	allowNegative: false,
+	stepHoldDelay: 500,
+	stepHoldInterval: t => Math.max(1000 / t ** 2, 25)
+}
+
+type EditCharacterModalProps = {
+	character: CharacterCardProps['character']
+	openedModal: boolean
+	closeModal: () => void
+}
+
+function EditCharacterModal({ character, openedModal, closeModal }: EditCharacterModalProps) {
+	const form = useForm({
+		mode: 'uncontrolled',
+		initialValues: {
+			currentShield: 10,
+			trauma: character.tabletopStats.trauma,
+			currentHealth: character.tabletopStats.health,
+			wounds: character.tabletopStats.wounds,
+			currentMovement: character.tabletopStats.movement
+		},
+		validate: {
+			currentShield: isInRange({ min: 0, max: character.stats.maxShield }, 'Shield must be between 0 and max shield'),
+			trauma: isInRange({ min: 0, max: character.stats.maxShield }, 'Trauma must be between 0 and max shield'),
+			currentHealth: isInRange({ min: 0, max: character.stats.maxHealth }, 'Health must be between 0 and max health'),
+			wounds: isInRange({ min: 0, max: character.stats.maxHealth }, 'Wounds must be between 0 and max health'),
+			currentMovement: isInRange({ min: 0, max: character.stats.maxMovement }, 'Movement must be between 0 and max movement')
+		}
+	})
+
+	const updateCharacter = useMutation({
+		mutationFn: updateCharacterAction,
+		onMutate: ({ data }) => {
+			// TODO optimistic update
+		},
+		onError: error => {
+			mutationError(error, 'Failed to update character')
+		}
+	})
+
+	const handleSubmit = (values: typeof form.values) => {
+		updateCharacter.mutate({ data: { tabletopCharacterId: character.tabletopCharacterId, values } })
+		closeModal()
+	}
+
+	return (
+		<Modal
+			opened={openedModal}
+			onClose={closeModal}
+			onExitTransitionEnd={form.reset}
+			title={`Editing ${character.characterName}`}
+			styles={{
+				header: {
+					zIndex: 0
+				}
+			}}
+		>
+			<form onSubmit={form.onSubmit(handleSubmit)}>
+				<Stack>
+
+					<Group grow align='flex-start'>
+						<NumberInput
+							label='Shield Durability'
+							key={form.key('currentShield')}
+							max={character.stats.maxShield}
+							{...form.getInputProps('currentShield')}
+							{...numberInputProps}
+						/>
+						<NumberInput
+							label='Trauma'
+							key={form.key('trauma')}
+							max={character.stats.maxShield}
+							{...form.getInputProps('trauma')}
+							{...numberInputProps}
+						/>
+						<NumberInput label='Max Shield' value={character.stats.maxShield} disabled />
+					</Group>
+					<Group grow align='flex-start'>
+						<NumberInput
+							label='Current Health'
+							key={form.key('currentHealth')}
+							max={character.stats.maxHealth}
+							{...form.getInputProps('currentHealth')}
+							{...numberInputProps}
+						/>
+						<NumberInput
+							label='Wounds'
+							key={form.key('wounds')}
+							max={character.stats.maxHealth}
+							{...form.getInputProps('wounds')}
+							{...numberInputProps}
+						/>
+						<NumberInput label='Max Health' value={character.stats.maxHealth} disabled />
+					</Group>
+					<Group>
+						<Button variant='default' onClick={closeModal}>Cancel</Button>
+						<Button flex={1} type='submit'>Update</Button>
+					</Group>
+				</Stack>
+			</form>
+		</Modal>
+	)
+}
+
+const updateCharacterSchema = type({
+	tabletopCharacterId: 'number',
+	values: {
+		currentShield: 'number',
+		trauma: 'number',
+		currentHealth: 'number',
+		wounds: 'number',
+		currentMovement: 'number'
+	}
+})
+
+const updateCharacterAction = createServerFn({ method: 'POST' })
+	.validator(updateCharacterSchema)
+	.handler(async ({ data: { tabletopCharacterId, values } }) => {
+		await requireGM({ tabletopCharacterId })
+
+		const { currentShield, trauma, currentHealth, wounds, currentMovement } = values
+
+		const serviceClient = getServiceClient()
+
+		const { error } = await serviceClient
+			.from('tabletop_characters')
+			.update({
+				shield: currentShield,
+				trauma,
+				health: currentHealth,
+				wounds,
+				movement: currentMovement
+			})
+			.eq('tt_character_id', tabletopCharacterId)
+		if (error) throw new Error(error.message, { cause: error })
+	})
